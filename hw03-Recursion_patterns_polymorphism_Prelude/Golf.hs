e3e86ce855b61a66b027fdff57c446a73fb37bc5
leopardshadow <thurtu811@gmail.com>
2020-04-13 19:23:34 +0800

    add hw3 exercise 2

--- hw03-Recursion_patterns_polymorphism_Prelude/Golf.hs
+++ hw03-Recursion_patterns_polymorphism_Prelude/Golf.hs
@@ -11,18 +11,21 @@ skips list = let steps = [ i | i <- [1 .. length list]] in
 indexAt:: [Int] -> [a] -> [a]
 indexAt indices list = [list!!x | x <- indices]
 
-
+-- TO-DO: Try using fold !!
 -- skips x = [ (foldr \i->{} [] [n | n <- [1, 1+m .. length x]] ) | m <- [1..length x]]
 
 -- erxrcise 2: Local maxima
+localMaxima :: [Integer] -> [Integer]
+localMaxima (n1:n2:n3:list)
+    | n1 < n2 && n2 > n3 = n2 : localMaxima restLst
+    | otherwise = localMaxima restLst
+    where restLst = (n2:n3:list)
+localMaxima _ = []
 
--- 
-
---localMaxima :: [Integer] -> [Integer]
---localMaxima 
-
-
-
+exercise2 = do 
+    print $ localMaxima [2,9,5,6,1] == [9,6]
+    print $ localMaxima [2,3,4,1,5] == [4]
+    print $ localMaxima [1,2,3,4,5] == []
 
 -- exercise 3: Histogram
 
