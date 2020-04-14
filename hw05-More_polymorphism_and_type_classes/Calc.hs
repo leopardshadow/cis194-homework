07bab073d9c36c5760753018cf3216c792e5de36
leopardshadow <thurtu811@gmail.com>
2020-04-14 15:50:40 +0800

    update hw5 ex2

--- hw05-More_polymorphism_and_type_classes/Calc.hs
+++ hw05-More_polymorphism_and_type_classes/Calc.hs
@@ -30,8 +30,31 @@ evalStr str = case (parseExp Lit Add Mul str) of
 testEx2 :: Bool
 testEx2 = and
     [
-        parseExp Lit Add Mul "(2+3)*4" == Just (Mul (Add (Lit 2) (Lit 3)) (Lit 4)),
-        parseExp Lit Add Mul "2+3*4" == Just (Add (Lit 2) (Mul (Lit 3) (Lit 4))),
-        parseExp Lit Add Mul "2+3*" == Nothing
+        evalStr "(2+3)*4" == Just 20, --Just (Mul (Add (Lit 2) (Lit 3)) (Lit 4)),
+        evalStr "2+3*4" == Just 14, --Just (Add (Lit 2) (Mul (Lit 3) (Lit 4))),
+        evalStr "2+3*" == Nothing
     ]
 
+
+-- Erercise 3
+
+-- class Expr a where
+--     lit :: a -> ExprT
+--     add :: ExprT -> ExprT -> ExprT
+--     mul :: ExprT -> ExprT -> ExprT
+
+-- instance Expr ExprT where
+
+--     lit :: Integer -> ExprT
+--     lit n = Lit n 
+
+--     add :: ExprT -> ExprT -> ExprT
+--     add e1 e2 = Add e1 e2
+
+--     mul :: ExprT -> ExprT -> ExprT
+--     mul e1 e2 = Mul e1 e2
+
+-- testEx3 = and
+--     [
+--         mul (add (lit 2) (lit 3)) (lit 4) :: ExprT == Mul (Add (Lit 2) (Lit 3)) (Lit 4)
+--     ]
