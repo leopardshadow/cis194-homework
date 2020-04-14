1bf1700b4f587fe4aa1c04d7c1b0158eca35ae1f
leopardshadow <thurtu811@gmail.com>
2020-04-14 13:17:16 +0800

    update hw3 ex3

--- hw03-Recursion_patterns_polymorphism_Prelude/Golf.hs
+++ hw03-Recursion_patterns_polymorphism_Prelude/Golf.hs
@@ -7,8 +7,8 @@ module Golf where
 
 skips :: [a] -> [[a]]
 skips list = let steps = [ i | i <- [1 .. length list]] in 
-              let ids = map (\step -> [step-1, 2*step-1 .. length list-1]) steps in
-                    [ (indexAt id list) | id <- ids]
+              let indices = map (\step -> [step-1, 2*step-1 .. length list-1]) steps in
+                    [ (indexAt index list) | index <- indices]
           
 indexAt:: [Int] -> [a] -> [a]
 indexAt indices list = [list!!x | x <- indices]
@@ -26,21 +26,29 @@ localMaxima (n1:n2:n3:list)
     where restLst = (n2:n3:list)
 localMaxima _ = []
 
+
+exercise2 :: Bool
 exercise2 = and
-	[
-		localMaxima [2,9,5,6,1] == [9,6]
-		localMaxima [2,3,4,1,5] == [4]
-		localMaxima [1,2,3,4,5] == []
-	]
+    [
+        localMaxima [2,9,5,6,1] == [9,6],
+        localMaxima [2,3,4,1,5] == [4],
+        localMaxima [1,2,3,4,5] == []
+    ]
 
 
 -- exercise 3: Histogram
 histogram :: [Integer] -> String
-histogram lst = let cntLst = count lst in
-                    unlines $ [ [ if (cntLst!!num >= line) then '*' else ' ' | num <- [0..9] ] | line <- [maximum cntLst, maximum cntLst-1 .. 1] ] ++ ["==========", "0123456789"]
+histogram lst = unlines $ [ [ if (cntLst!!num >= line) then '*' else ' ' | num <- [0..9] ] | line <- liness  ] ++ legends
+                    where cntLst = count lst
+                          liness = [maximum cntLst, maximum cntLst-1 .. 1] -- from top bottom
+                          legends = ["==========", "0123456789"]
+
 
 count :: [Integer] -> [Int]
 count lst = [ length $ filter (==n) lst | n <- [0..9] ]
--- traverse the list 10 times better solution ??
 
+{-
+putStr $ histogram [1,4,5,4,6,6,3,4,2,4,9]
+putStr $ histogram [1,1,1,5]
+-}
 
