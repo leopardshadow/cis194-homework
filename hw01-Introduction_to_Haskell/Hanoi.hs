a192f50aa25446b6fb897ba324b95f6951545d6a
leopardshadow <thurtu811@gmail.com>
2020-04-14 11:11:40 +0800

    add hw1 ex6, BUT not done yet

--- hw01-Introduction_to_Haskell/Hanoi.hs
+++ hw01-Introduction_to_Haskell/Hanoi.hs
@@ -4,6 +4,7 @@
 
 type Peg = String
 type Move = (Peg, Peg)
+
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
 hanoi n src dst tmp = fromSrc2Tmp ++ fromSrc2Dst ++ fromTmp2Dst
@@ -11,5 +12,14 @@ hanoi n src dst tmp = fromSrc2Tmp ++ fromSrc2Dst ++ fromTmp2Dst
                               fromSrc2Dst = [(src, dst)]
                               fromTmp2Dst = hanoi (n-1) tmp dst src
 
--- testEx5 :: bool
--- testEx5 = hanoi 2 "a" "b" "c" == [("a","c"), ("a","b"), ("c","b")]
+-- length $ hanoi 15 "a" "b" "c"
+
+-- Exercise 6
+
+
+-- hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+-- hanoi4 0 _ _ _ _ = []
+-- hanoi4 n src dst tmp1 tmp2 = fromSrc2Tmp ++ fromSrc2Dst ++ fromTmp2Dst
+--                                 where fromSrc2Tmp = hanoi4 (n-1) src tmp dst
+--                                       fromSrc2Dst = [(src, dst)]
+--                                       fromTmp2Dst = hanoi4 (n-1) tmp dst src
