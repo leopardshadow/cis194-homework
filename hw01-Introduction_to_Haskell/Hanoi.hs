623ad479f002ec7f828ff2f1a17df95f8519ac04
leopardshadow <thurtu811@gmail.com>
2020-04-16 10:51:27 +0800

    add hw1 ex6

--- hw01-Introduction_to_Haskell/Hanoi.hs
+++ hw01-Introduction_to_Haskell/Hanoi.hs
@@ -16,10 +16,17 @@ hanoi n src dst tmp = fromSrc2Tmp ++ fromSrc2Dst ++ fromTmp2Dst
 
 -- Exercise 6
 
+--           n          k       src    dst    tmp1   tmp2         
+hanoi4 :: Integer -> Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoi4 1 _ src dst _ _ = [ (src, dst) ]
+hanoi4 n k src dst tmp1 tmp2 = mFromSrc2Tmp ++ restFromSrc2Dst ++ mFromTmp2Dst
+                                where n' = fromIntegral n :: Double
+                                      m  = n - round ( sqrt ( (2*n'+1) ) ) + 1
+                                      leftN = n - m
+                                      mFromSrc2Tmp    = hanoi4 m k src tmp1 dst tmp2
+                                      restFromSrc2Dst = hanoi leftN src dst tmp2
+                                      mFromTmp2Dst    = hanoi4 m k tmp1 dst src tmp2
 
--- hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
--- hanoi4 0 _ _ _ _ = []
--- hanoi4 n src dst tmp1 tmp2 = fromSrc2Tmp ++ fromSrc2Dst ++ fromTmp2Dst
---                                 where fromSrc2Tmp = hanoi4 (n-1) src tmp dst
---                                       fromSrc2Dst = [(src, dst)]
---                                       fromTmp2Dst = hanoi4 (n-1) tmp dst src
+
+
+-- hanoi4 15 4 "a" "b" "c" "d"
