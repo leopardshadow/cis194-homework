6737748f6d1f1709f37bda91b06a25695075dcf5
leopardshadow <thurtu811@gmail.com>
2020-04-17 15:28:45 +0800

    edit hw6 ex3-5

--- hw06-Lazy_evaluation/Fibonacci.hs
+++ hw06-Lazy_evaluation/Fibonacci.hs
@@ -36,8 +36,11 @@ streamToList :: Stream a -> [a]
 streamToList (Cons x xs) = x : streamToList xs
 
 -- show :: a -> String
+-- instance Show a => Show (Stream a) where
+--     show s = foldr (\x y -> x ++ " " ++ y ) [] (map show (take 20 $ streamToList s))
+
 instance Show a => Show (Stream a) where
-    show s = foldr (\x y -> x ++ " " ++ y ) [] (map show (take 20 $ streamToList s))
+    show = show . take 20 . streamToList
 
 
 -- test case
@@ -66,16 +69,22 @@ nats :: Stream Integer
 nats = streamFromSeed (+1) 0
 
 
+-- ruler :: Stream Integer
+-- ruler = streamMap timesDiv2 (streamFromSeed (+1) 1)
+
+
+-- timesDiv2 :: Integer -> Integer
+-- timesDiv2 n 
+--     | n `mod` 2 == 0 = 1 + timesDiv2 ( n `div` 2 )
+--     | otherwise = 0
+
+
 ruler :: Stream Integer
-ruler = streamMap timesDiv2 (streamFromSeed (+1) 1)
+ruler = streamInterleave (streamRepeat 0) (streamMap (+1) ruler)
 
+streamInterleave :: Stream a -> Stream a -> Stream a
+streamInterleave (Cons x xs) ys = Cons x (streamInterleave ys xs)
 
-timesDiv2 :: Integer -> Integer
-timesDiv2 n 
-    | n `mod` 2 == 0 = 1 + timesDiv2 ( n `div` 2 )
-    | otherwise = 0
-
--- interleaveStreams
 
 
 -- Exercise 6
