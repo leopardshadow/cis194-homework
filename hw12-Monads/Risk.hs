afbbdb845aa1a35f107ef3ab6a4b426b0e5dee63
leopardshadow <thurtu811@gmail.com>
2020-05-05 11:38:41 +0800

    update hw12 ex2-3

--- hw12-Monads/Risk.hs
+++ hw12-Monads/Risk.hs
@@ -3,6 +3,8 @@
 module Risk where
 
 import Control.Monad.Random
+import Data.List (sortBy)
+
 
 ------------------------------------------------------------
 -- Die values
@@ -31,13 +33,47 @@ data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
 
 -- Exercise 2
 
--- battle :: Battlefield -> Rand StdGen Battlefield
--- battle bf = 
+--
+atkUnit :: Army -> Army
+atkUnit n = min 3 (n-1)
+
+defUnit :: Army -> Army
+defUnit = min 2
+
+--
+rollDices :: Army -> Rand StdGen [DieValue]
+rollDices n = replicateM n die 
+
+--
+sortDices :: [DieValue] -> [DieValue]
+sortDices = sortBy (flip compare)
+
+--
+matchDices :: [DieValue] -> [DieValue] -> [Ordering]
+matchDices = zipWith compare
+
+-- 
+updateBf :: Battlefield -> [Ordering] -> Battlefield
+updateBf (Battlefield atk def) xs = (Battlefield atk def)
+    where atkDeath = length . filter (==GT) $ xs
+          defDeath = length . filter (/=GT) $ xs
+          atk' = atk - atkDeath 
+          def' = def - defDeath
+
+--
+battle :: Battlefield -> Rand StdGen Battlefield
+battle bf@(Battlefield atk def) = do
+    atkDices <- sortDices <$> rollDices (atkUnit atk)
+    defDices <- sortDices <$> rollDices (defUnit def)
+    return $ updateBf bf (matchDices atkDices defDices)
 
 
-maxAtk :: Army -> Int
-maxAtk n = min 3 (n-1)
 
-maxDef :: Army -> Int
-maxDef = min 2
+-- Exercise 3
+
+invade :: Battlefield -> Rand StdGen Battlefield
+invade = undefined
+
+endGame :: Battlefield -> Bool
+endGame (Battlefield atk def) = (atk < 2) || (def == 0)
 
